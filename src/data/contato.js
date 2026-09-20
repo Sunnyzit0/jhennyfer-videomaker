5f7c421e155c50b963c68f504b9409e8d541a01b
Sunny <arthurgapb@gmail.com>
2026-09-20 20:41:16 -0300

    fix: adicionar número de WhatsApp da cliente

--- src/data/contato.js
+++ src/data/contato.js
@@ -1,6 +1,6 @@
 export const contato = {
-  whatsappNumber: 'SEU_NUMERO_AQUI',
-  whatsappDisplay: '(XX) XXXXX-XXXX',
+  whatsappNumber: '556198386233',
+  whatsappDisplay: '(61) 9838-6233',
   whatsappMessage: 'Oi, Jhennyfer! Vim pelo seu site e gostaria de conversar sobre um projeto.',
   instagramHandle: '@SEU_INSTAGRAM',
   instagramLink: 'https://instagram.com/SEU_INSTAGRAM',
