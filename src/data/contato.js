b1c29ec740b16aa5ed4636ee3d37877f7c8b4b13
Sunny <arthurgapb@gmail.com>
2026-09-20 20:50:08 -0300

    fix: adicionar instagram da cliente

--- src/data/contato.js
+++ src/data/contato.js
@@ -2,6 +2,6 @@ export const contato = {
   whatsappNumber: '556198386233',
   whatsappDisplay: '(61) 9838-6233',
   whatsappMessage: 'Oi, Jhennyfer! Vim pelo seu site e gostaria de conversar sobre um projeto.',
-  instagramHandle: '@SEU_INSTAGRAM',
-  instagramLink: 'https://instagram.com/SEU_INSTAGRAM',
+  instagramHandle: '@jhennyfermonteir_',
+  instagramLink: 'https://www.instagram.com/jhennyfermonteir_/',
 }
