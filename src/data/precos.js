bf8379c328807c8bb7efdf56c73b5b99312c698b
Sunny <arthurgapb@gmail.com>
2026-09-20 20:41:27 -0300

    feat: adicionar dados de valores em precos.js

---
+++ src/data/precos.js
@@ -0,0 +1,22 @@
+export const precos = [
+  {
+    servico: 'Videomaker',
+    inclui: ['{item incluso}', '{item incluso}'],
+    valor: '{valor}',
+  },
+  {
+    servico: 'Fotografia',
+    inclui: ['{item incluso}', '{item incluso}'],
+    valor: '{valor}',
+  },
+  {
+    servico: 'Criação de conteúdo',
+    inclui: ['{item incluso}', '{item incluso}'],
+    valor: '{valor}',
+  },
+  {
+    servico: 'Divulgação',
+    inclui: ['{item incluso}', '{item incluso}'],
+    valor: '{valor}',
+  },
+]
